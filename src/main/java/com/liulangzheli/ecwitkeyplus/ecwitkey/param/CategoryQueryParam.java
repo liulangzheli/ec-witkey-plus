ef1bad107ed438edbf52e3a49c2ecc50277b0abb
liulangzheli <79363508@qq.com>
2019-12-19 21:58:20 +0800

    类别管理增加类别性质过滤条件

--- src/main/java/com/liulangzheli/ecwitkeyplus/ecwitkey/param/CategoryQueryParam.java
+++ src/main/java/com/liulangzheli/ecwitkeyplus/ecwitkey/param/CategoryQueryParam.java
@@ -1,6 +1,7 @@
 package com.liulangzheli.ecwitkeyplus.ecwitkey.param;
 
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
@@ -21,4 +22,8 @@ import com.liulangzheli.ecwitkeyplus.common.param.OrderQueryParam;
 @ApiModel(value = "CategoryQueryParam对象", description = "类别管理查询参数")
         public class CategoryQueryParam extends OrderQueryParam {
         private static final long serialVersionUID = 1L;
+
+        @ApiModelProperty(value = "类别性质")
+        private int categoryType;
+
 }
